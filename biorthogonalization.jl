a7aceec25271a3bfeb4baf3c45a1aaa6d0d73590
GabrielClave <gabriel.r.clave@gmail.com>
2026-03-13 11:58:47 +0100

    biconjugate gradient

--- biorthogonalization.jl
+++ biorthogonalization.jl
@@ -80,20 +80,120 @@ function tridiagonal_biorthogonalization!(
     return(maxiter)
 end
 
+struct LanczosWorkspace{T}
+    V::Matrix{T}
+    W::Matrix{T}
+    T_mat::Tridiagonal{T}
+end
+
+# A "Constructor"
+function LanczosWorkspace(m::Int, max_k::Int, T=Float64)
+    V = zeros(T, m, max_k)
+    W = zeros(T, m, max_k)
+    alpha = zeros(T, max_k)
+    beta = zeros(T, max_k - 1)
+    gamma = zeros(T, max_k - 1)
+    return LanczosWorkspace(V, W, Tridiagonal(beta, alpha, gamma))
+end
+
 m = 300
 max_k = 100
 A = randn(m, m)
+lw = LanczosWorkspace(m, max_k)
 
-V = zeros(m,max_k)
-W = zeros(m,max_k)
-alpha = zeros(max_k)
-beta_sub = zeros(max_k - 1)
-gamma_up = zeros(max_k - 1)
-T_mat = Tridiagonal(beta_sub, alpha, gamma_up)
+# n = tridiagonal_biorthogonalization!(A, lw.V, lw.W, lw.T_mat, maxiter = max_k)
 
-n = tridiagonal_biorthogonalization!(A, V, W, T_mat, maxiter = max_k)
-
-println("Bi-orthonormality (||W'v - I||): ", norm(view(W, :, 1:n)' * view(V, :, 1:n) - I))
+# println("Bi-orthonormality (||W'v - I||): ", norm(view(lw.W, :, 1:n)' * view(lw.V, :, 1:n) - I))
 # 20: yes
 # 50: not quite !
 # 100: garbage
+
+# biconjugate gradient
+# solve Ax = b
+# does not minimize |r_n|
+# instead enforces r_n ⟂ K(A*,b)
+
+function bcg(
+    A::AbstractMatrix{T},
+    b::AbstractVector{T};
+    maxiter::Int = 50, 
+    tol::T = 1e-12
+) where {T<:AbstractFloat}
+    m, _ = size(A)
+    x = zeros(T, m)
+    
+    # Initialization: r = b - Ax (assumes x=0)
+    r = copy(b)
+    s = copy(b) # Shadow residual r*
+    p = copy(r)
+    q = copy(s) # Shadow search direction p*
+    
+    Ap = zeros(T, m)
+    Atq = zeros(T, m)
+    
+    rho_prev = dot(s, r)
+    residual = norm(r)
+
+    for k in 1:maxiter
+
+        mul!(Ap, A, p)
+        mul!(Atq, A', q)
+        
+        denom = dot(q, Ap) # qnApn
+        
+        if abs(denom) < tol
+            println("serious breackdown after $k steps. residual: $residual")
+            return x, k
+        end
+        
+        alpha = rho_prev / denom
+
+        x .+= alpha .* p
+        r .-= alpha .* Ap
+        s .-= alpha .* Atq
+
+        residual = norm(r)
+
+        if norm(r) < tol
+            println("Converged in $k steps.")
+            return x, k
+        end
+
+        rho_cur = dot(s, r)
+
+        if abs(rho_cur) < tol
+            println("inner product is zero at step $k, algortithm can't progress. residual: $residual")
+            return x, k
+        end
+        
+        beta = rho_cur / rho_prev
+        
+        p .= r .+ beta .* p
+        q .= s .+ beta .* q
+        
+        rho_prev = rho_cur
+    end
+
+    println("hit $maxiter iterations. residual: $residual")
+    return x, maxiter
+end
+
+# bad matrix
+m = 300
+A = randn(m, m)
+b = randn(m)
+
+x, n = bcg(A,b)
+
+# good matrix
+m = 300
+max_k = 100
+X = randn(m, m)/sqrt(m)
+A = X'*X + 5I + randn(m, m)/sqrt(m)
+A[1,1] = 12
+b = randn(m)
+
+x, n = bcg(A,b)
+# beta = 0 after around 10 steps
+# at that point dot(s, r) ≃ norm(r)²
+# because the tolerance is e-12, we stop at rnorm(r) ≃ e-6
